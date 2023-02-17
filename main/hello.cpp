f55f9af6e0926c88c48c33b06d0c8251a47d46c9
Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>
2023-02-17 13:06:26 +0530

    Test for webhooks
    Signed-off-by: Adarsh Liju Abraham <adarsh.liju.abraham@gmail.com>

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "I was created by Adarsh Liju Abraham";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Testing the webhooks";
   return 0;
 }
